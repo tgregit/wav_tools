f5554066332ba37f63b2cc491da041aae31f9233
tgregit <tgregory747@gmail.com>
2018-02-18 11:15:30 -0500

    seconde versions

--- wav_test.py
+++ wav_test.py
@@ -1,8 +1,11 @@
 import wav_tools as wt
 
 path_to_vctk_data = '/home/foo/data/VCTK-Corpus'
-vc = wt.vctk(path_to_vctk_data)
-print(vc.data_path)
+vctk = wt.vctk(path_to_vctk_data)
+
+wav = vctk.get_wav(255,2)
+
+#print(vc.data_path)
 
 
 # wt.foo()
--- wav_tools.py
+++ wav_tools.py
@@ -4,11 +4,28 @@ import numpy as np
 
 class vctk:
     data_path = './'
+    sample_rate = 22050
 
     def __init__(self, data_path):
         self.data_path = data_path
 
+    def get_number_string_from_int(self, my_int):
+        length = len(str(my_int))
+        zeros_to_prepend = 3 - length
+        zeros = '0' * zeros_to_prepend
+        num_string = zeros + str(my_int)
 
+        return num_string
+
+
+    def get_wav(self, speaker_id, sentence_id):
+        speaker_prefix = 'p' + str(speaker_id)
+
+        number_string = self.get_number_string_from_int(sentence_id)
+
+        full_wav_path = self.data_path + '/wav48/' + speaker_prefix + '/' + speaker_prefix + '_' + number_string
+
+        print(full_wav_path)
 
 
 def foo():
