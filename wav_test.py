faf03b477def47f23b587a48e6786ddfd119f74b
tgregit <tgregory747@gmail.com>
2018-02-18 11:20:59 -0500

    third ver, actual loads wav

--- wav_test.py
+++ wav_test.py
@@ -4,6 +4,7 @@ path_to_vctk_data = '/home/foo/data/VCTK-Corpus'
 vctk = wt.vctk(path_to_vctk_data)
 
 wav = vctk.get_wav(255,2)
+print(wav)
 
 #print(vc.data_path)
 
--- wav_tools.py
+++ wav_tools.py
@@ -18,12 +18,18 @@ class vctk:
         return num_string
 
 
-    def get_wav(self, speaker_id, sentence_id):
+    def get_wav(self, speaker_id, sentence_id, strip_silence=False):
         speaker_prefix = 'p' + str(speaker_id)
-
         number_string = self.get_number_string_from_int(sentence_id)
+        full_wav_path = self.data_path + '/wav48/' + speaker_prefix + '/' + speaker_prefix + '_' + number_string + '.wav'
 
-        full_wav_path = self.data_path + '/wav48/' + speaker_prefix + '/' + speaker_prefix + '_' + number_string
+        data, sr = librosa.load(full_wav_path, self.sample_rate)
+
+        if strip_silence:
+            print('strip sielcne here')
+
+        return data
+
 
         print(full_wav_path)
 
