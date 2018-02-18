a2185c7701095c269563d00d2d5ad83d563b711b
tgregit <tgregory747@gmail.com>
2018-02-18 10:49:46 -0500

    first simple version of test file

---
+++ wav_test.py
@@ -0,0 +1,8 @@
+import wav_tools as wt
+
+path_to_vctk_data = '/home/foo/data/VCTK-Corpus'
+vc = wt.vctk(path_to_vctk_data)
+print(vc.data_path)
+
+
+# wt.foo()
---
+++ wav_tools.py
@@ -0,0 +1,15 @@
+import librosa
+import numpy as np
+
+
+class vctk:
+    data_path = './'
+
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+
+
+
+def foo():
+    print('foo')
