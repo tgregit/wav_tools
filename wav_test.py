576ae3b89a8de3e3e4fb785fae08eb2b2dc87e02
tgregit <tgregory747@gmail.com>
2018-02-19 17:59:01 -0500

    strip silence changes

--- wav_test.py
+++ wav_test.py
@@ -3,16 +3,18 @@ from scipy.fftpack import fft, dct, idct
 import librosa
 import numpy as np
 window_length = 1024
-coef_to_keep = 256
+coef_to_keep = 256#128#512#256
 
 vctk = wt.vctk('/home/foo/data/VCTK-Corpus')
 
-wav = vctk.get_wav(228, 2)  # speaker #255, sentence #2  #wav = wt.normalize(wav)
-print(wav[1110:1115])
+wav = vctk.get_wav(228, 2, True)  # speaker #255, sentence #2  #wav = wt.normalize(wav) T
+
 librosa.output.write_wav('/home/foo/data/wavs-test/28.wav', wav, sr=vctk.sample_rate)
 
-dct_compressed = wt.wav_to_dct_coef_compressed(wav, window_length, coef_to_keep)
-i = wt.dct_coef_compressed_to_amplitudes(dct_compressed, window_length)
-print(i, i.shape)
-
-librosa.output.write_wav('/home/foo/data/wavs-test/28idct.wav', i, sr=vctk.sample_rate)
+# dct_compressed = wt.wav_to_dct_coef_compressed(wav, window_length, coef_to_keep)
+# for dc in dct_compressed:
+#     print(dc)
+# i = wt.dct_coef_compressed_to_amplitudes(dct_compressed, window_length)
+# print(i, i.shape)
+#
+# librosa.output.write_wav('/home/foo/data/wavs-test/28idct.wav', i, sr=vctk.sample_rate)
--- wav_tools.py
+++ wav_tools.py
@@ -18,6 +18,33 @@ class vctk:
 
         return num_string
 
+    def strip_silence(self, my_data, my_window_length):
+        total_windows = my_data.shape[0] // my_window_length
+        start_index = 0
+        end_index = my_data.shape[0]
+
+        std_threshold = .008
+
+        for i in range(0, total_windows):
+            current_window = my_data[i*my_window_length:(i*my_window_length) + my_window_length]
+            if current_window.std() > std_threshold:
+                start_index = i * my_window_length
+                break
+
+        for i in range(total_windows - 1, 0, -1):
+            current_window = my_data[i * my_window_length:(i * my_window_length) + my_window_length]
+            print(current_window.std())
+            if current_window.std() > std_threshold:
+                end_index = (i * my_window_length)
+                break
+        data_silence_stripped = my_data[start_index:end_index]
+
+        print(start_index,end_index)
+
+        return data_silence_stripped
+
+
+
     def get_wav(self, speaker_id, sentence_id, strip_silence=False):
         speaker_prefix = 'p' + str(speaker_id)
         number_string = self.get_number_string_from_int(sentence_id)
@@ -25,7 +52,9 @@ class vctk:
 
         data, sr = librosa.load(full_wav_path, self.sample_rate)
 
-        if strip_silence:
+        if strip_silence == True:
+            data = self.strip_silence(data, 2048)
+
             print('strip silence here')
 
         return data
@@ -39,8 +68,8 @@ def normalize(my_data):
 
 
 def normalize_neg_one(my_data):
-    my_data = my_data - my_data.min()
-    my_data = my_data / (my_data.max())
+    my_data = my_data - (my_data.min() * 1.0)
+    my_data = my_data / ((my_data.max()) * 1.0)
     my_data = my_data - .5
     my_data = my_data * 2.0
 
@@ -48,8 +77,7 @@ def normalize_neg_one(my_data):
 
 
 def wav_to_dct_coef_compressed(my_wav, my_window_length, my_coef_to_keep):
-    total_windows  = my_wav.shape[0] // my_window_length
-
+    total_windows = my_wav.shape[0] // my_window_length
     compressed_dct = np.zeros((total_windows, my_coef_to_keep))
 
     my_wav = my_wav[0:total_windows * my_window_length]
@@ -72,7 +100,7 @@ def dct_coef_compressed_to_amplitudes(my_dct_compressed, my_window_length):
 
     for i in range(0, total_windows):
         compressed_row = my_dct_compressed[i]
-        dct_padded[i][0: coef_keep] = compressed_row   #np.zeros has already padded the ends with 0.0's
+        dct_padded[i][0: coef_keep] = compressed_row   # np.zeros has already padded the ends with 0.0's
 
     inverse_dct = idct(dct_padded, norm='ortho') # these are now amplitudes
 
