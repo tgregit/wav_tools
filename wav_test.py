7a7dd94f4987c38ceef8f72ae200f7902e4fc5b0
tgregit <tgregory747@gmail.com>
2018-02-19 12:14:05 -0500

    started dct coef compression

--- wav_test.py
+++ wav_test.py
@@ -1,10 +1,22 @@
 import wav_tools as wt
+from scipy.fftpack import fft, dct, idct
 
 path_to_vctk_data = '/home/foo/data/VCTK-Corpus'
 vctk = wt.vctk(path_to_vctk_data)
 
-wav = vctk.get_wav(255,2)  # speaker #255, sentence #2
-print(wav)
+wav = vctk.get_wav(228,2)  # speaker #255, sentence #2
+
+
+# print(wav.shape[0])
+# dct = dct(wav) / 20.0
+# print(dct[0:100])
+
+window_length = 1024
+coef_to_keep = 170
+
+d = wt.wav_to_dct_coef(wav, window_length, coef_to_keep)
+print(d)
+
 
 
 #print(vc.data_path)
--- wav_tools.py
+++ wav_tools.py
@@ -1,5 +1,6 @@
 import librosa
 import numpy as np
+from scipy.fftpack import fft, dct, idct
 
 
 class vctk:
@@ -17,7 +18,7 @@ class vctk:
 
         return num_string
 
-    def     get_wav(self, speaker_id, sentence_id, strip_silence=False):
+    def get_wav(self, speaker_id, sentence_id, strip_silence=False):
         speaker_prefix = 'p' + str(speaker_id)
         number_string = self.get_number_string_from_int(sentence_id)
         full_wav_path = self.data_path + '/wav48/' + speaker_prefix + '/' + speaker_prefix + '_' + number_string + '.wav'
@@ -30,8 +31,16 @@ class vctk:
         return data
 
 
-        print(full_wav_path)
+def wav_to_dct_coef(my_wav, my_window_length, my_coef_to_keep):
+    total_windows  = my_wav.shape[0] // my_window_length
+    dct_coef = np.zeros((total_windows, my_coef_to_keep), dtype=np.float)
 
+    for i in range(0, total_windows):
+        window = my_wav[i*my_window_length:((i*my_window_length) + my_window_length)]
+        window_dct = dct(window)
+        dct_coef[i] = window_dct[0:my_coef_to_keep]
+
+    return dct_coef
 
 
 
